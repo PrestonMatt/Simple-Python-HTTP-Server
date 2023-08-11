749757b2e5227cb62ee3ba9eecd1758924451dee
Matthew Preston <46906782+PrestonMatt@users.noreply.github.com>
2023-08-11 12:45:18 -0400

    Finished Project.

--- Answers/client_answer.py
+++ Answers/client_answer.py
@@ -10,37 +10,22 @@ from urllib.parse import urlparse
 import ssl
 import http.client
 import json
+import subprocess
 
-def test_connection():
-    _keyfile = "/home/user/Desktop/Simple-Python-HTTP-Server/local_keys/myCA.key"
-    _certfile = "/home/user/Desktop/Simple-Python-HTTP-Server/local_keys/myCA.pem"
+def test_connection(passwd:str):
     # https://docs.python.org/3/library/http.client.html
 
     url = "127.0.0.1"
     port = 443
-    HTTP_Methods = ["GET","POST"]
+    HTTP_Methods = ["GET","POST","HEAD"]
     for method in HTTP_Methods:
-        context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-        context.load_cert_chain(
-            certfile=_certfile,
-            keyfile=_keyfile,
-            password="YOUR_CERT_PASSWORD_HERE!"
-        )
+
         ssl._create_default_https_context = ssl._create_unverified_context
 
         http_client = http.client.HTTPSConnection(
             url,
             port
         )
-        """
-        http_client.sock = context.wrap_socket(
-            sock=http_client.sock,
-            server_side=False,
-            do_handshake_on_connect=True,
-            suppress_ragged_eofs=False,
-            server_hostname=None,
-        )
-        """
 
         headers = {'Content-type': 'application/json'}
         data = {'JUNK':'Testing...'}
@@ -52,32 +37,115 @@ def test_connection():
             headers
         )
         response = http_client.getresponse()
-        print("Status: {} and reason: {}".format(response.status, response.reason))
+        print("Method: {}, status: {} and reason: {}".format(method, response.status, response.reason))
 
         http_client.close()
+    
+    print("\n\n(Note, both GET and POST requests are JUNK requests and should return 400)")
 
-    """
-    with requests.Session() as session:
-        #retry = Retry(connect=3, backoff_factor=0.5)
-        #adapter = HTTPAdapter(max_retries=retry)
-        #session.mount('http://', adapter)
-        #session.mount('https://', adapter)
+def retrieve_password():
+    # ~THIS IS TERRIBLE SECURITY, DON'T EVER DO THIS~
+    with open("/home/user/Desktop/Simple-Python-HTTP-Server/local_keys/openssl_passphrase.txt","r") as passfd:
+        print("Retreiving password...")
+        password = passfd.readlines()[0].strip("\n")
+    #print("Test...the password is: %s" % password)
+    return password
 
-        header = "GET /file.txt HTTP/1.1"
+def get_cmd(mthd:str,filename:str) -> bool:
 
-        
-        print("Starting the test")
-        response = session.get(url, verify=False)
-        response.close()
-        print("Test response:\n\t%s" % response.content)"""
+    url = "127.0.0.1"
+    port = 443
+    header = {}
+    _data = {}
+    if(mthd == "GET"):
+        header = {'Connection': 'file checkout'}
+        _data = {'Trying to request file:':filename[1:]}
+    elif(mthd == "POST"):
+        header = {'Connection': 'file check back in'}
+        content = b""
+        try:
+            with open(filename.strip(".txt") + ".temp","rb") as confd:
+                lines = confd.readlines()
+                for line in lines:
+                    content += line
+            # delete file
+            print("Deleting temporary file for editting.")
+            x = subprocess.Popen(
+                ["rm",filename.strip(".txt") + ".temp"],
+                stderr=subprocess.PIPE,
+                stdout=subprocess.PIPE
+            )
+            x.kill()
+        except FileNotFoundError:
+            print("File does not exist! Pushing empty contents to server.")
+        _data = {
+            'Trying to push file:':filename[1:],
+            'Content':content.decode('utf8')
+        }
+    
+    data_json = json.dumps(_data)
+
+    #ssl._create_default_https_context = ssl._create_unverified_context
+
+    http_client = http.client.HTTPSConnection(
+        url,
+        port,
+    )
+
+    http_client.request(
+        mthd,
+        filename,
+        data_json,
+        header
+    )
+    response = http_client.getresponse()
+    #print(response)
+    print("Method: {}, status: {} and reason: {}".format(mthd, response.status, response.reason))
+
+    #print(response.headers)
+
+    http_client.close()
+
+    if(response.status == 200 and mthd == "GET"):
+        # extract the file data
+        with open(filename.strip(".txt") + ".temp","w") as confd:
+            # Get the file content and copy to temp file:
+            resp_headrs = response.headers
+            
+            resp_headrs = str(resp_headrs).split("File-Content:")[1]
+            resp_headrs = resp_headrs[3:-2]
+            
+            resp_headrs = resp_headrs.replace("\\n","\n")
+            resp_headrs = resp_headrs.replace("\\t","\t")
+            resp_headrs = resp_headrs.replace("'b'","")
+            resp_headrs = resp_headrs.replace("'","")
+
+            confd.write(str(resp_headrs))
+            # allow user to write new data
+            print("Please write the appending lines you want to add to the file:")
+            try:
+                while(True):
+                    line = input("#")
+                    confd.write(line)
+            except KeyboardInterrupt:
+                print("\nDone writing lines!")
+        return True
+    elif(response.status == 200 and mthd == "POST"):
+        return True
+    return False
 
 def process(cmd:str) -> bool:
     if(cmd.lower() == "q" or cmd.lower == "quit"):
         return False
     else:
-        #body = cmd
-        response = requests.get(URL,verify=False)
-        print("Server Response:\n%s" % response)
+        filename = "/" + cmd.split(" ")[1]
+        print(filename)
+        if(cmd.lower().__contains__("get")):
+            success = get_cmd("GET",filename)
+            print("Your GET request succeeded: %s" % success)
+        elif(cmd.lower().__contains__("post")):
+            success = get_cmd("POST",filename)
+            print("Your POST request succeeded: %s" % success)
     return True
 
 def loop():
@@ -87,8 +155,12 @@ def loop():
         breakLoop = process(cmd)
 
 def main():
-    test_connection()
-    #loop()
+    try:
+        password = retrieve_password()
+        test_connection(password)
+        loop()
+    except ConnectionRefusedError:
+        print("No connection found to server!")
     print("Quitting!")
 
 if __name__ == "__main__":
--- Answers/server_answer.py
+++ Answers/server_answer.py
@@ -1,16 +1,43 @@
 #import sys
-#import os
+from os.path import exists
 import requests as rqst
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse
 import ssl
 import time
+import json
 #import socket
 
 class RequestHandler(BaseHTTPRequestHandler):
 
+    locks = {
+        "":False,
+        "/":False,
+        "JUNK":False
+    }
+
+    def update_locks(self):
+        with open("locks.json","w") as locksfd:
+            locksfd.write(json.dumps(self.locks))
+
+    def do_HEAD(self):
+        print("Recieved a POST HTTP request.")
+        content_len = self.headers.get('Content-Length')
+        try:
+            request_body = self.rfile.read(int(content_len))
+            print(request_body.decode("utf8"))
+        except Exception as err:
+            return
+        
+        message = "Test"
+        self.protocol_version = "HTTP/1.1"
+        self.send_response(200)
+        self.send_header("Content-Length", len(message))
+        self.end_headers()
+        self.wfile.write(bytes(message, "utf8"))
+
     def do_POST(self):
-        print("Handling POST")
+        print("Recieved a POST HTTP request.")
         content_len = self.headers.get('Content-Length')
         try:
             request_body = self.rfile.read(int(content_len))
@@ -21,17 +48,115 @@ class RequestHandler(BaseHTTPRequestHandler):
         """
         Process POST Request
         """
-        message = "Test!"
-        print("Recieved a GET HTTP request.")
+
+        """
         self.protocol_version = "HTTP/1.1"
         self.send_response(200)
         self.send_header("Content-Length", len(message))
         self.end_headers()
         self.wfile.write(bytes(message, "utf8"))
+        """
+
+        message = "<p>Trying to put your fileback."
+
+        # If the resource is in use, allow a person to check it back in
+        try:
+            req = request_body.decode("utf8")
+            req = req.split("Trying to push file")[1] # cut off the first part
+            req = req.split(",")[0] # cut out the content
+            req = req[5:]
+            req = req[:-1]
+            filename = req
+        except IndexError:
+            message += "\nThere is some error with your filename. Please try again"
+            message += "<\p>"
+            self.protocol_version = "HTTP/1.1"
+            self.send_response(400)
+            self.send_header("Content-Length", len(message))
+            self.end_headers()
+            self.wfile.write(bytes(message, "utf8"))
+            return
+        
+        #print(filename)
+        # Get the content:
+        try:
+            req = request_body.decode("utf8")
+            req = req.split("Content\": \"")[1] # cut off the first part
+            req = req[:-2] # cut off the "} at the end
+            content = req
+        except IndexError:
+            message += "\nThere is some error with your filename. Please try again"
+            message += "<\p>"
+            self.protocol_version = "HTTP/1.1"
+            self.send_response(400)
+            self.send_header("Content-Length", len(message))
+            self.end_headers()
+            self.wfile.write(bytes(message, "utf8"))
+            return
+        
+        #print(content)
+
+        # Check if the file exists
+        extant_bool = exists(filename)
+        if(extant_bool):
+            message += "\nFile exists"
+            
+            try:
+                if(self.locks[filename] != True):
+                    pass
+            except:
+                self.locks[filename] = False
+                message += "\nLooks like the file exists, but hasn't been accessed yet."
+                message += "<\p>"
+                self.protocol_version = "HTTP/1.1"
+                self.send_response(409)
+                self.send_header("Content-Length", len(message))
+                self.end_headers()
+                self.wfile.write(bytes(message, "utf8"))
+                # first time accessing the file
+                return
+
+            if(self.locks[filename] == True):
+                # Step 3: If there is a lock, remove one on the file
+                self.locks[filename] = False
+                # Step 4: Allow user to edit file.
+                message += "\nAll good! Editting checked back in."
+                with open(filename,"w") as filed:
+                    con = content.replace("\\n","\n")
+                    con = con.replace("\\t","\t")
+                    # write what was given to the file
+                    #filed.write(bytes(content,'utf8'))
+                    filed.write(con)
+                message += "<\p>"
+                self.protocol_version = "HTTP/1.1"
+                self.send_response(200)
+                self.send_header("Content-Length", len(message))
+                self.end_headers()
+                self.wfile.write(bytes(message, "utf8"))
+            else:
+                message += "\nLooks like the file isn't in use. Try again later"
+                message += "<\p>"
+                self.protocol_version = "HTTP/1.1"
+                self.send_response(412)
+                self.send_header("Content-Length", len(message))
+                self.end_headers()
+                self.wfile.write(bytes(message, "utf8"))
+        else:
+            message += "\nLooks like the file does not exist. I cannot put something back that does not exist."
+            message += "<\p>"
+            self.protocol_version = "HTTP/1.1"
+            self.send_response(404)
+            self.send_header("Content-Length", len(message))
+            self.end_headers()
+            self.wfile.write(bytes(message, "utf8"))
+
+        self.update_locks()
+
         return
 
     def do_GET(self):
-        print("Handling GET")
+        print("Recieved a GET HTTP request.")
+
         content_len = self.headers.get('Content-Length')
         try:
             request_body = self.rfile.read(int(content_len))
@@ -39,20 +164,88 @@ class RequestHandler(BaseHTTPRequestHandler):
         except Exception as err:
             return
         
+        message = "<p>Attempting to retrieve that file for you!"
+
         """
-        Process GET Request
-        """
-        message = input("Say something>") #"Test!"
-        print("Recieved a GET HTTP request.")
         self.protocol_version = "HTTP/1.1"
         self.send_response(200)
         self.send_header("Content-Length", len(message))
         self.end_headers()
         self.wfile.write(bytes(message, "utf8"))
+        """
+
+        """
+        Process GET Request
+        """
+        # Step 1: See if the file exists, if not, create it
+        filename = ""
+        try:
+            req = request_body.decode("utf8").split("Trying to request file")[1]
+            req = req[5:]
+            req = req[:-2]
+            filename = req
+        except IndexError:
+            message += "\nThere is some error with your filename. Please try again"
+            message += "<\p>"
+            self.protocol_version = "HTTP/1.1"
+            self.send_response(400)
+            self.send_header("Content-Length", len(message))
+            self.end_headers()
+            self.wfile.write(bytes(message, "utf8"))
+            return
+
+        extant_bool = exists(filename)
+        if(extant_bool):
+            message += "\nFile exists"
+            # Step 2: See if there is a lock on the file (someone else using it)
+            
+            try:
+                if(self.locks[filename] != True):
+                    pass
+            except:
+                # first time accessing the file
+                self.locks[filename] = False
+
+            if(self.locks[filename] != True):
+                # Step 3: If no lock, place one on the file
+                self.locks[filename] = True
+                # Step 4: Allow user to edit file.
+                message += "\nAll good! Start editting."
+                contents = ""
+                with open(filename,"rb") as filed:
+                    lines = filed.readlines()
+                    for line in lines:
+                        contents += str(line)
+                message += "<\p>"
+                self.protocol_version = "HTTP/1.1"
+                self.send_response(200)
+                self.send_header("Content-Length", len(message))
+                self.send_header("File-Content",contents)
+                self.end_headers()
+                self.wfile.write(bytes(message, "utf8"))
+            else:
+                message += "\nLooks like the file is in use. Try again later"
+                message += "<\p>"
+                self.protocol_version = "HTTP/1.1"
+                self.send_response(226)
+                self.send_header("Content-Length", len(message))
+                self.end_headers()
+                self.wfile.write(bytes(message, "utf8"))
+        else:
+            message += "\nLooks like the file does not exist. I cannot loan out something to you that does not exist."
+            message += "<\p>"
+            self.protocol_version = "HTTP/1.1"
+            self.send_response(404)
+            self.send_header("Content-Length", len(message))
+            self.end_headers()
+            self.wfile.write(bytes(message, "utf8"))
+
+        self.update_locks()
+
         return
 
 def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    requests = 10
+    requests = 20
     #https://docs.python.org/3/library/ssl.html
 
     # If you want to use the build in Ubuntu keys, you can change the following lines to be uncommented:
@@ -65,7 +258,7 @@ def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
     # https://windowsreport.com/create-self-signed-certificate/#:~:text=Use%20OpenSSL%20to%20create%20a%20self-signed%20certificate%201,created%20earlier%20for%20the%20public%2Fprivate%20key%20file.%20
     
     server = ("127.0.0.1", 443)
-
+    #passwd = retrieve_password()
     # Open up the server
     # https://docs.python.org/3/library/http.server.html
     
@@ -75,7 +268,7 @@ def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
     context.load_cert_chain(
         certfile=_certfile,
         keyfile=_keyfile,
-        password="YOUR_CERT_PASSWORD_HERE!"
+        password="YOU PASSWORD HERE!"
     )
     ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -89,11 +282,19 @@ def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
     
     while(requests > 0):
         requests -= 1
-        print("Handling a HTTP request.")
+        print("\n\nWaiting for a new HTTP request.")
         #time.sleep(3)
         http_serv.handle_request()
     
     print("All out of requests!")
+
+def retrieve_password():
+    # ~THIS IS TERRIBLE SECURITY, DON'T EVER DO THIS~
+    with open("/home/user/Desktop/Simple-Python-HTTP-Server/local_keys/openssl_passphrase.txt","r") as passfd:
+        print("Retreiving password...")
+        password = passfd.readlines()[0].strip("\n")
+    #print("Test...the password is: %s" % password)
+    return password
 
 def main():
     run()
